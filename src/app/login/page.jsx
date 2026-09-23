0c1e904093509c7bb252c7ca857debe498d385ed
mdsohelrana599 <mdsohelrana812005@gmail.com>
2026-09-23 19:55:51 +0600

    2nd

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import Titel from "@/components/Titel";
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <Titel>WelCome to Login Page</Titel>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,29 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div class="navbar bg-base-100 shadow-sm p-5 border-b-3">
-      <a class="btn btn-ghost text-xl"> Dev Story</a>
+    <div className="navbar bg-base-100 shadow-sm flex px-9 py-5 border-b-2">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Dev-story</a>
+      </div>
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            aria-label="More"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current"
+          >
+            {" "}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            ></path>{" "}
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
